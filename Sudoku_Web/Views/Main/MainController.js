e60c308a9c147d568fce193ef3691447c9fd70c3
praveen <praveen@gmail.com>
2016-09-01 17:52:28 +0530

    third commit

--- Sudoku_Web/Views/Main/MainController.js
+++ Sudoku_Web/Views/Main/MainController.js
@@ -13,6 +13,18 @@
         [7,0,3,0,1,8,0,0,0]
     ];
 
+    $scope.flag = [
+    [0, 0, 0, 2, 6, 0, 7, 0, 1],
+    [6, 8, 0, 0, 7, 0, 0, 9, 0],
+    [1, 9, 0, 0, 0, 4, 5, 0, 0],
+    [8, 2, 0, 1, 0, 0, 0, 4, 0],
+    [0, 0, 4, 6, 0, 2, 9, 0, 0],
+    [0, 5, 0, 0, 0, 3, 0, 2, 8],
+    [0, 0, 9, 3, 0, 0, 0, 7, 4],
+    [0, 4, 0, 0, 5, 0, 0, 3, 6],
+    [7, 0, 3, 0, 1, 8, 0, 0, 0]
+    ];
+
     $scope.Answer = [
         [4, 3, 5, 2, 6, 9, 7, 8, 1],
         [6, 8, 2, 5, 7, 1, 4, 9, 3],
@@ -26,11 +38,12 @@
     ];
 
     $scope.Validate = function (Question,Answer) {
-        $window.alert(angular.equals(Question,Answer));
+        msg = angular.equals(Question, Answer);
+        if (msg == "true")
+            $window.alert("success");
+        else
+            $window.alert("not success");
     };
 
-    $scope.setSolution = function ($i,$j,Question,value) {
-        console.log($, $j);
-        Question[$i][$j] = value;
-    }
+    
 }]);
